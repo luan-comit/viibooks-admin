dc9fa41ff38c67bbe2a0705118d78c027ddb9596
luan-comit <fptnet1280@yahoo.com>
2025-08-08 19:44:25 -0600

    updated logo size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
       <div className="flex items-center gap-2 text-xs">
         <img
           onClick={() => navigate("/")}
-          className="w-36 sm:w-40 cursor-pointer"
+          className="w-36 sm:w-20 cursor-pointer"
           src={assets.admin_logo}
           alt=""
         />
